9c517dcbdc8a6a05583158ad6d397cc2bf219ac2
kairoLamarca <lamarca.kairo@gmail.com>
2017-11-09 13:23:19 -0200

    component
    implementação de varias maneiras de chamar os components

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-topo',
-    templateUrl: './topo.component.html'
+    //selector: '[app-topo]',//no html fica <div app-topo></app-topo>
+    //selector: '.app-topo',//funciona como class -- não pode ter uma classe com o mesmo nome no css
+    templateUrl: './topo.component.html',
+    //template: `<p>
+    //                Esse é o componente topo novo
+    //            </p>`//template não precisa de um arquivo -- Crase possibilita quebrar linha
+    // styles: [`
+    //         .exemplo { 
+    //             color: red 
+    //         }
+    //         '`]//este estilo sera usado somente no component referenciado junto
+    styleUrls: ['./topo.component.css']
 })
-export class TopoComponent {}
+export class TopoComponent { }
