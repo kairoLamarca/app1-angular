0449faa950f86f8564c554f90152ca95ad2d3c5e
kairoLamarca <lamarca.kairo@gmail.com>
2017-11-09 13:54:37 -0200

    component progresso e tentativas
    utilização do generate para criar os components progresso e tentativas

---
+++ src/app/progresso/progresso.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-progresso',
+  templateUrl: './progresso.component.html',
+  styleUrls: ['./progresso.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ProgressoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/tentativas/tentativas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-tentativas',
+  templateUrl: './tentativas.component.html',
+  styleUrls: ['./tentativas.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class TentativasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
