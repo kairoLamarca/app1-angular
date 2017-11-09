cbf4b4e066398fd03d8d438fa86642b8ff1734de
kairoLamarca <lamarca.kairo@gmail.com>
2017-11-09 13:46:45 -0200

    component painel
    crianção co componenet usando ng generate

---
+++ src/app/painel/painel.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-painel',
+  templateUrl: './painel.component.html',
+  styleUrls: ['./painel.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class PainelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
