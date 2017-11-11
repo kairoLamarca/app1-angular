8ce1bd98e8fd1ead42a7ec84275f1fdadb62a5bc
kairolamarca <lamarca.kairo@gmail.com>
2017-11-11 17:51:25 -0200

    verificação de frase
    verifica se a frase esta correta e troca pra proxima rodada

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -13,15 +13,39 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:';
-  public resposta: string 
+  public resposta: string
 
-  constructor() { console.log(this.frases) }
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase)
+  }
 
   ngOnInit() {
   }
 
-  public atualizaResposta(resposta: Event): void{
+  public atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value;
-    console.log(this.resposta);
+  }
+
+  public verificarResposta(): void {
+
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('A tradução está correta');
+
+      //trocar pergunta da rodada
+      this.rodada++;
+
+      //console.log(this.rodada);
+      //atualiza o objeto rodadaFrase
+      this.rodadaFrase = this.frases[this.rodada];
+
+      //console.log(this.rodadaFrase);
+
+    } else {
+      alert('A tradução está errada');
+    }
   }
 }
